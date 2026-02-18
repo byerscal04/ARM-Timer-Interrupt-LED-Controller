a835d68652a253933bb12df1103b11be9cd077a2
byerscal04 <byerscal04@gmail.com>
2026-02-18 04:34:52 -0800

    Update asm_main.asm

--- asm_main.asm
+++ asm_main.asm
@@ -1,14 +1,15 @@
-    .thumb
-    .text
-    .global asm_main
-    .ref PortF_Init
-    .ref Timer2A_Init
+.thumb
+.text
+.global asm_main
+.ref PortF_Init
+.ref Timer2A_Init
 
 asm_main:
+
     push {lr}
 
     bl PortF_Init
     bl Timer2A_Init
 
 Forever:
-    b   Forever
+    b Forever
